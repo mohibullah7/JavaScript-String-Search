846619fd0ad9fbb2bf7c088eb0adb3ae5fd5bf38
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-05-18 17:26:01 +0500

    match methods match all
    practiced strings methods

---
+++ stringMethods.js
@@ -0,0 +1,19 @@
+// yesterdday we learn about search method string functions How we can searc a particular string value and its index value using search methid iundexOF method 
+
+let ab = 'foyund the value in this string "value " and console it';
+console.log(ab.search('value '));
+
+// search method search value from left to right index 
+
+let vc="with this var is used to found 'with' indexOf method found it 8";
+console.log(vc.indexOf("with",10 ));
+
+//the indexOf method resturn the substring value and 
+
+//now let check match string using js 
+let mch="this is javascript and this  is goinis ";
+let mchVal=mch.match(/is/)
+document.getElementById("demo").innerHTML=mch.length,"",mchVal;
+console.log(mchVal);
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,2 @@
-let mtch="this is string to match values 900 009 maintain in thin coming";
-let matchit=mtch.match('in');
-console.log(matchit,"",mtch);
+let mch="this is javascript and this  is goinis ";
+console.log(mch.match(/is/g));
