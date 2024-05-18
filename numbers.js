9d9ae51d7f23b7ca1ce101144c75a663b55fe8da
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-05-19 01:57:42 +0500

    numbers and more about templets for loop
    number s and for lops

---
+++ numbers.js
@@ -0,0 +1,11 @@
+let num = 34.000e-5;
+let b = 0.0000;
+let a = 9e4
+localStorage
+console.log(num+b);
+
+// e is write to add zero to left for big number and -5 to add from left decimal point 
+
+let g = 0.2;
+let h = 0.1;
+console.log(g+h);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,3 @@
-let func = `this is used to create function ${function add(){
-  let a = 40;
-  b = 50;
-  c = 80;
-  d= a+b+c;
-  let addit=d;
-  console.log(addit);
-}
- } his is function of addition ${add()} `
-
- console.log(func);
+let g = 0.2;
+let h = 0.1;
+console.log(g+h);
